2fb1d135ee346db540fa1ce255c05d413bbeffc5
linling <996396597@qq.com>
2021-07-26 03:03:23 +0000

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,14 +5,15 @@
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
   </head>
   <body>
-    @include('layouts._header');
+    @include('layouts._header')
 
     <div class="container">
         <div class="offse-md-1 col-md-10">
             @include('shared._messages')
             @yield('content')
             @include('layouts._footer')
+        </div>
     </div>
-    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
