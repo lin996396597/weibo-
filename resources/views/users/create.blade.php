228e9cdf62d5e32f6fe14e292bbff72f2d8ef59b
linling <996396597@qq.com>
2021-07-26 02:13:08 +0000

    添加了CSRF验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,29 +8,30 @@
       <h5>注册</h5>
     </div>
     <div class="card-body">
-      <form method="POST" action="{{ route('users.store') }}">
-          <div class="form-group">
+        <form method="POST" action="{{ route('users.store') }}">
+            {{ csrf_field() }}
+            <div class="form-group">
             <label for="name">名称：</label>
             <input type="text" name="name" class="form-control" value="{{ old('name') }}">
-          </div>
+            </div>
 
-          <div class="form-group">
+            <div class="form-group">
             <label for="email">邮箱：</label>
             <input type="text" name="email" class="form-control" value="{{ old('email') }}">
-          </div>
+            </div>
 
-          <div class="form-group">
+            <div class="form-group">
             <label for="password">密码：</label>
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
-          </div>
+            </div>
 
-          <div class="form-group">
+            <div class="form-group">
             <label for="password_confirmation">确认密码：</label>
             <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
-          </div>
+            </div>
 
-          <button type="submit" class="btn btn-primary">注册</button>
-      </form>
+            <button type="submit" class="btn btn-primary">注册</button>
+        </form>
     </div>
   </div>
 </div>
